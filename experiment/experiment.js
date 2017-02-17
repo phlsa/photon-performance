2c3a0dfa23a7e3d5c7fcc597823f9ef2c8e2aa0f
Philipp Sackl <philipp@mozilla.com>
2017-02-17 13:12:55 +0100

    improve instruction handling

--- experiment/experiment.js
+++ experiment/experiment.js
@@ -1,3 +1,23 @@
+function instruction(content, vars) {
+  clearInstructions();
+  var instruction = document.createElement('div');
+  instruction.className = "exp-instruction";
+
+  instruction.innerHTML = content.join(' ');
+
+  // handle extra vars
+  if (vars) {
+    if (vars.nextButton) {
+      var nb = document.createElement('button');
+      nb.innerText = vars.nextButtonLabel || "Next Step";
+      nb.className = "exp-button";
+      nb.addEventListener('click', vars.nextButton);
+      instruction.appendChild(nb);
+    }
+  }
+  document.body.appendChild(instruction);
+}
+
 function ask(content, vars) {
   var overlay = document.createElement('div');
   overlay.className = "exp-overlay";
@@ -21,4 +41,10 @@ function scale() {
 
 function text(txt) {
   return "<div class='text'>"+ txt +"</div>";
+}
+
+function clearInstructions() {
+  Array.prototype.slice.call(document.querySelectorAll('.exp-instruction, .exp-overlay'), 0).forEach(item => {
+    item.parentNode.removeChild(item);
+  })
 }
